6f60c1d3d28295ae73bb28d7592439b5562f518b
Maciej Wolf <maciej.wolf7@gmail.com>
2025-04-10 15:46:21 +0200

    added styles and initial script

---
+++ script.js
@@ -0,0 +1,17 @@
+const activeFilters = document.querySelector('#active-filters');
+const filterButton = document.querySelectorAll('.filter-button');
+const extensionList = document.querySelector('#extension-list');
+const headerBar = document.querySelector('#header-bar');
+
+let theme;
+let selectedFilter;
+
+headerBar.addEventListener("click", function(e) {
+    if (e.target && e.target.matches('button')) {
+        
+    }
+})
+
+function handleFilterChange () {
+
+}
