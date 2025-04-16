eb4a00427aae5946e20975aad6c66bf8147bbba7
Maciej Wolf <maciej.wolf7@gmail.com>
2025-04-16 10:15:19 +0200

    added switching between list filters

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const extensionList = document.querySelector('#extension-list');
 const headerBar = document.querySelector('#header-bar');
 
 let theme;
-let selectedFilter;
+let selectedFilter = "all-filter";
+let extensions = [];
 
-headerBar.addEventListener("click", function(e) {
+activeFilters.addEventListener("click", function(e) {
     if (e.target && e.target.matches('button')) {
-        
+        selectedFilter = e.target.id;
+        filterButton.forEach(btn => btn.classList.remove("selected"));
+        e.target.classList.add("selected");
+        // handleFilterChange(e.target.id);
+        console.log(selectedFilter);
     }
-})
+});
 
-function handleFilterChange () {
+// function handleFilterChange (buttonId) {
+//     filterButton.forEach(btn => btn.classList.remove("selected"));
 
-}
+// }
